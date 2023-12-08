ebd6e48489f5e649b3f98e241fdca16d33a003e7
Mikey P <138417934+vppelli@users.noreply.github.com>
2023-12-08 14:03:03 -0500

    Updated main-view
    Removed unused code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -12,7 +12,6 @@ export const MainView = () => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
     const storedToken = localStorage.getItem("token");
     const [movies, setMovies] = useState([]);
-    const [selectedMovie, setSelectedMovie] = useState(null);
     const [user, setUser] = useState(null);
     const [token, setToken] = useState(null);
 
@@ -48,52 +47,6 @@ export const MainView = () => {
             });
     }, [token]);
 
-//     return (
-//         <Row className="justify-content-md-center"> 
-//             {!user ? (
-//                 <Col md = {5}>
-//                     <LoginView
-//                         onLoggedIn = {(user, token) => {
-//                             setUser(user);
-//                             setToken(token);
-//                         }}
-//                     />
-//                     or
-//                     <SignupView 
-//                     />
-//                 </Col>
-//             ) : selectedMovie ? (
-//                 <Row className="justify-content-md-center">
-//                     <MovieView style = {{ border: "1px solid white" }} movie = { selectedMovie } onBackClick = {() => setSelectedMovie(null)} />
-//                 </Row>
-//             ) : movies.length === 0 ? (
-//                     <Row>The list is empty!</Row>
-//             ) : (
-//                 <Row>
-//                     <Button
-//                         onClick = {() => {
-//                             setUser(null);
-//                             setToken(null);
-//                             localStorage.clear();
-//                         }}
-//                     >
-//                         Logout
-//                     </Button>
-//                     {movies.map((movie) => (
-//                         <Col className = "mb-5" key = { movie.id } md = {3}>
-//                             <MovieCard
-//                                 movie = { movie }
-//                                 onMovieClick = {(newSelectedMovie) => {
-//                                     setSelectedMovie(newSelectedMovie);
-//                                 }}
-//                             />
-//                         </Col>
-//                     ))}
-//                 </Row>
-//             )}
-//         </Row>
-//     );
-// };
     return (
         <BrowserRouter>
             <NavigationBar
