b760ce5c50f920819ef62144ec7a44018d7543ae
Mikey P <138417934+vppelli@users.noreply.github.com>
2024-01-15 18:18:40 -0500

    Updated movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { BookmarkPlus, BookmarkCheckFill } from "react-bootstrap-icons";
